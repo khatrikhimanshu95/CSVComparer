df017d980ae05ec6e44aabd1a5445f2c43d0a78e
Himanshu Khatri <khatrik.himanshu95@gmail.com>
2013-04-17 23:01:21 +0530

    CSV Comparer
    CSV Comparer

---
+++ CSVComparer/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace CSVComparer {
+   partial class Form1 {
+      /// <summary>
+      /// Required designer variable.
+      /// </summary>
+      private System.ComponentModel.IContainer components = null;
+
+      /// <summary>
+      /// Clean up any resources being used.
+      /// </summary>
+      /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+      protected override void Dispose(bool disposing) {
+         if (disposing && (components != null)) {
+            components.Dispose();
+         }
+         base.Dispose(disposing);
+      }
+
+      #region Windows Form Designer generated code
+
+      /// <summary>
+      /// Required method for Designer support - do not modify
+      /// the contents of this method with the code editor.
+      /// </summary>
+      private void InitializeComponent() {
+         this.panel1 = new System.Windows.Forms.Panel();
+         this.dataGridView1 = new System.Windows.Forms.DataGridView();
+         this.lblTRecords1 = new System.Windows.Forms.Label();
+         this.label4 = new System.Windows.Forms.Label();
+         this.panel2 = new System.Windows.Forms.Panel();
+         this.dataGridView2 = new System.Windows.Forms.DataGridView();
+         this.lblTRecords2 = new System.Windows.Forms.Label();
+         this.label6 = new System.Windows.Forms.Label();
+         this.label3 = new System.Windows.Forms.Label();
+         this.panel3 = new System.Windows.Forms.Panel();
+         this.dataGridView3 = new System.Windows.Forms.DataGridView();
+         this.button1 = new System.Windows.Forms.Button();
+         this.button2 = new System.Windows.Forms.Button();
+         this.button3 = new System.Windows.Forms.Button();
+         this.label1 = new System.Windows.Forms.Label();
+         this.label2 = new System.Windows.Forms.Label();
+         this.panel1.SuspendLayout();
+         ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+         this.panel2.SuspendLayout();
+         ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+         this.panel3.SuspendLayout();
+         ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+         this.SuspendLayout();
+         // 
+         // panel1
+         // 
+         this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+         this.panel1.Controls.Add(this.dataGridView1);
+         this.panel1.Location = new System.Drawing.Point(16, 40);
+         this.panel1.Name = "panel1";
+         this.panel1.Size = new System.Drawing.Size(605, 261);
+         this.panel1.TabIndex = 0;
+         // 
+         // dataGridView1
+         // 
+         this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+         this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+         this.dataGridView1.Name = "dataGridView1";
+         this.dataGridView1.Size = new System.Drawing.Size(597, 253);
+         this.dataGridView1.TabIndex = 0;
+         // 
+         // lblTRecords1
+         // 
+         this.lblTRecords1.AutoSize = true;
+         this.lblTRecords1.Location = new System.Drawing.Point(146, 13);
+         this.lblTRecords1.Name = "lblTRecords1";
+         this.lblTRecords1.Size = new System.Drawing.Size(0, 13);
+         this.lblTRecords1.TabIndex = 1;
+         // 
+         // label4
+         // 
+         this.label4.AutoSize = true;
+         this.label4.Location = new System.Drawing.Point(14, 13);
+         this.label4.Name = "label4";
+         this.label4.Size = new System.Drawing.Size(120, 13);
+         this.label4.TabIndex = 0;
+         this.label4.Text = "Total Records in this file";
+         // 
+         // panel2
+         // 
+         this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+         this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+         this.panel2.Controls.Add(this.dataGridView2);
+         this.panel2.Location = new System.Drawing.Point(643, 40);
+         this.panel2.Name = "panel2";
+         this.panel2.Size = new System.Drawing.Size(605, 261);
+         this.panel2.TabIndex = 3;
+         // 
+         // dataGridView2
+         // 
+         this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+         this.dataGridView2.Location = new System.Drawing.Point(3, 3);
+         this.dataGridView2.Name = "dataGridView2";
+         this.dataGridView2.Size = new System.Drawing.Size(597, 253);
+         this.dataGridView2.TabIndex = 1;
+         // 
+         // lblTRecords2
+         // 
+         this.lblTRecords2.AutoSize = true;
+         this.lblTRecords2.Location = new System.Drawing.Point(782, 13);
+         this.lblTRecords2.Name = "lblTRecords2";
+         this.lblTRecords2.Size = new System.Drawing.Size(0, 13);
+         this.lblTRecords2.TabIndex = 3;
+         // 
+         // label6
+         // 
+         this.label6.AutoSize = true;
+         this.label6.Location = new System.Drawing.Point(640, 13);
+         this.label6.Name = "label6";
+         this.label6.Size = new System.Drawing.Size(120, 13);
+         this.label6.TabIndex = 2;
+         this.label6.Text = "Total Records in this file";
+         // 
+         // label3
+         // 
+         this.label3.AutoSize = true;
+         this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.label3.Location = new System.Drawing.Point(13, 314);
+         this.label3.Name = "label3";
+         this.label3.Size = new System.Drawing.Size(120, 22);
+         this.label3.TabIndex = 4;
+         this.label3.Text = "Result Output";
+         // 
+         // panel3
+         // 
+         this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+         this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+         this.panel3.Controls.Add(this.dataGridView3);
+         this.panel3.Location = new System.Drawing.Point(16, 349);
+         this.panel3.Name = "panel3";
+         this.panel3.Size = new System.Drawing.Size(1232, 240);
+         this.panel3.TabIndex = 1;
+         // 
+         // dataGridView3
+         // 
+         this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+         this.dataGridView3.Location = new System.Drawing.Point(3, 3);
+         this.dataGridView3.Name = "dataGridView3";
+         this.dataGridView3.Size = new System.Drawing.Size(1224, 232);
+         this.dataGridView3.TabIndex = 1;
+         // 
+         // button1
+         // 
+         this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.button1.Location = new System.Drawing.Point(139, 307);
+         this.button1.Name = "button1";
+         this.button1.Size = new System.Drawing.Size(201, 36);
+         this.button1.TabIndex = 5;
+         this.button1.Text = "Export Result To CSV";
+         this.button1.UseVisualStyleBackColor = true;
+         this.button1.Visible = false;
+         this.button1.Click += new System.EventHandler(this.button1_Click);
+         // 
+         // button2
+         // 
+         this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.button2.Location = new System.Drawing.Point(420, 3);
+         this.button2.Name = "button2";
+         this.button2.Size = new System.Drawing.Size(201, 32);
+         this.button2.TabIndex = 6;
+         this.button2.Text = "Upload CSV First File";
+         this.button2.UseVisualStyleBackColor = true;
+         this.button2.Visible = false;
+         this.button2.Click += new System.EventHandler(this.button2_Click);
+         // 
+         // button3
+         // 
+         this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+         this.button3.Location = new System.Drawing.Point(1047, 3);
+         this.button3.Name = "button3";
+         this.button3.Size = new System.Drawing.Size(201, 32);
+         this.button3.TabIndex = 7;
+         this.button3.Text = "Upload CSV Second File";
+         this.button3.UseVisualStyleBackColor = true;
+         this.button3.Visible = false;
+         this.button3.Click += new System.EventHandler(this.button3_Click);
+         // 
+         // label1
+         // 
+         this.label1.AutoSize = true;
+         this.label1.Location = new System.Drawing.Point(244, 13);
+         this.label1.Name = "label1";
+         this.label1.Size = new System.Drawing.Size(68, 13);
+         this.label1.TabIndex = 8;
+         this.label1.Text = "Processing...";
+         this.label1.Visible = false;
+         // 
+         // label2
+         // 
+         this.label2.AutoSize = true;
+         this.label2.Location = new System.Drawing.Point(861, 12);
+         this.label2.Name = "label2";
+         this.label2.Size = new System.Drawing.Size(68, 13);
+         this.label2.TabIndex = 9;
+         this.label2.Text = "Processing...";
+         this.label2.Visible = false;
+         // 
+         // Form1
+         // 
+         this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+         this.ClientSize = new System.Drawing.Size(1272, 607);
+         this.Controls.Add(this.lblTRecords2);
+         this.Controls.Add(this.lblTRecords1);
+         this.Controls.Add(this.label6);
+         this.Controls.Add(this.label2);
+         this.Controls.Add(this.label4);
+         this.Controls.Add(this.label1);
+         this.Controls.Add(this.button3);
+         this.Controls.Add(this.button2);
+         this.Controls.Add(this.button1);
+         this.Controls.Add(this.panel3);
+         this.Controls.Add(this.label3);
+         this.Controls.Add(this.panel2);
+         this.Controls.Add(this.panel1);
+         this.Name = "Form1";
+         this.Text = "CSV Comparer";
+         this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+         this.panel1.ResumeLayout(false);
+         ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+         this.panel2.ResumeLayout(false);
+         ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+         this.panel3.ResumeLayout(false);
+         ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+         this.ResumeLayout(false);
+         this.PerformLayout();
+
+      }
+
+      #endregion
+
+      private System.Windows.Forms.Panel panel1;
+      private System.Windows.Forms.Panel panel2;
+      private System.Windows.Forms.Label label3;
+      private System.Windows.Forms.Panel panel3;
+      private System.Windows.Forms.Button button1;
+      private System.Windows.Forms.Button button2;
+      private System.Windows.Forms.Button button3;
+      private System.Windows.Forms.Label label1;
+      private System.Windows.Forms.Label label2;
+      private System.Windows.Forms.Label lblTRecords1;
+      private System.Windows.Forms.Label label4;
+      private System.Windows.Forms.Label lblTRecords2;
+      private System.Windows.Forms.Label label6;
+      private System.Windows.Forms.DataGridView dataGridView1;
+      private System.Windows.Forms.DataGridView dataGridView2;
+      private System.Windows.Forms.DataGridView dataGridView3;
+   }
+}
+
---
+++ CSVComparer/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CSVReaderWriter;
+namespace CSVComparer {
+   public partial class Form1 : Form {
+      private string CSVFilePath1;
+      private string CSVFilePath2;
+      private List<string> CSVFile1Headers;
+      private List<string> CSVFile2Headers;
+      private List<List<string>> RecordsOfCSVFile1;
+      private List<List<string>> RecordsOfCSVFile2;
+      private string CSVOutputFile;
+      public Form1(string args) {
+         InitializeComponent();
+         if (!string.IsNullOrEmpty(args)) {
+            var files = args.ToString().Split(',');
+            CSVFilePath1 = files[0].ToString();
+            CSVFilePath2 = files[1].ToString();
+            CSVOutputFile = files[2].ToString();
+            button2_Click(null, null);
+            button3_Click(null, null);
+            button1_Click(null, null);
+         }
+      }
+      public Form1() {
+         InitializeComponent();
+      }
+
+      /// <summary>
+      /// Upload First CSV File
+      /// </summary>
+      /// <param name="sender"></param>
+      /// <param name="e"></param>
+      private void button2_Click(object sender, EventArgs e) {
+         try {
+            label2.Visible = false;
+            string filePath = string.Empty;
+            if (string.IsNullOrEmpty(CSVFilePath1)) {
+               OpenFileDialog openFileD1 = new OpenFileDialog();
+               openFileD1.Filter = "CSV Files (*.csv)|*.csv|All files (*.*)|*.*;";// "CSV files (*.csv)";
+               openFileD1.Multiselect = false;
+               if (openFileD1.ShowDialog() == DialogResult.OK) {
+                  label1.Visible = true;
+                  string ext = Path.GetExtension(openFileD1.FileName.Trim());
+                  if (ext.ToLower() != ".csv") {
+                     MessageBox.Show("Please upload a CSV file");
+                     return;
+                  }
+                  filePath = openFileD1.FileName.Trim();
+               }
+               else {
+                  return;
+               }
+            }
+            else {
+               filePath = CSVFilePath1;
+            }
+            using (CsvReader reader = new CsvReader(filePath, Encoding.Default)) {
+               RecordsOfCSVFile1 = new List<List<string>>();
+               while (reader.ReadNextRecord())
+                  RecordsOfCSVFile1.Add(reader.Fields);
+               if (RecordsOfCSVFile1.Count > 1) {
+                  CsvFile csvFile = CreateCsvFile(RecordsOfCSVFile1[0], RecordsOfCSVFile1[1]);
+                  CSVFile1Headers = csvFile.Headers;
+               }
+            }
+            if (RecordsOfCSVFile1.Count > 0) {
+               lblTRecords1.Text = (RecordsOfCSVFile1.Count - 1).ToString();
+               DataTable dtTable = ConvertListToDataTable(RecordsOfCSVFile1.Skip(1).ToList());
+               dataGridView1.DataSource = dtTable.DefaultView;
+            }
+            label1.Text = "Processed";
+            label1.Visible = true;
+         }
+         catch (Exception ex) {
+         }
+      }
+
+      /// <summary>
+      /// Upload Second CSV File
+      /// </summary>
+      /// <param name="sender"></param>
+      /// <param name="e"></param>
+      private void button3_Click(object sender, EventArgs e) {
+         try {
+            label2.Visible = false;
+            string filePath = string.Empty;
+            if (string.IsNullOrEmpty(CSVFilePath2)) {
+               OpenFileDialog openFileD1 = new OpenFileDialog();
+               openFileD1.Filter = "CSV Files (*.csv)|*.csv|All files (*.*)|*.*;";// "CSV files (*.csv)";
+               openFileD1.Multiselect = false;
+               if (openFileD1.ShowDialog() == DialogResult.OK) {
+                  string ext = Path.GetExtension(openFileD1.FileName.Trim());
+                  if (ext.ToLower() != ".csv") {
+                     MessageBox.Show("Please upload a CSV file");
+                     return;
+                  }
+                  filePath = openFileD1.FileName.Trim();
+               }
+               else {
+                  return;
+               }
+            }
+            else {
+               filePath = CSVFilePath2;
+            }
+
+            label2.Visible = true;
+            using (CsvReader reader = new CsvReader(filePath, Encoding.Default)) {
+               RecordsOfCSVFile2 = new List<List<string>>();
+               while (reader.ReadNextRecord())
+                  RecordsOfCSVFile2.Add(reader.Fields);
+               if (RecordsOfCSVFile2.Count > 1) {
+                  CsvFile csvFile = CreateCsvFile(RecordsOfCSVFile2[0], RecordsOfCSVFile2[1]);
+                  CSVFile2Headers = csvFile.Headers;
+               }
+            }
+
+            if (RecordsOfCSVFile2.Count > 0) {
+               lblTRecords2.Text = (RecordsOfCSVFile2.Count - 1).ToString();
+               DataTable dtTable = ConvertListToDataTable(RecordsOfCSVFile2.Skip(1).ToList());
+               dataGridView2.DataSource = dtTable.DefaultView;
+            }
+            label2.Text = "Processed";
+         }
+         catch (Exception ex) {
+
+         }
+      }
+
+      /// <summary>
+      /// Create a new CSV file from the result
+      /// </summary>
+      /// <param name="sender"></param>
+      /// <param name="e"></param>
+      private void button1_Click(object sender, EventArgs e) {
+         try {
+            List<string> diffOfHeaders = new List<string>();
+            if (CSVFile2Headers.Count <= CSVFile1Headers.Count)
+               diffOfHeaders = CSVFile1Headers.Except(CSVFile2Headers).ToList();
+            else
+               diffOfHeaders = CSVFile2Headers.Except(CSVFile1Headers).ToList();
+
+            List<List<string>> results = new List<List<string>>();
+
+            if (RecordsOfCSVFile1.Count <= RecordsOfCSVFile2.Count) {
+               for (int i = 0; i <= RecordsOfCSVFile2.Count - 1; i++) {
+                  if (i <= RecordsOfCSVFile1.Count - 1) {
+                     for (int k = 0; k <= RecordsOfCSVFile2[i].Count - 1; k++) {
+                        if (RecordsOfCSVFile2[i][k] != RecordsOfCSVFile1[i][k]) {
+                           results.Add(RecordsOfCSVFile2[i]);
+                        }
+                     }
+                  }
+                  else {
+                     results.Add(RecordsOfCSVFile2[i]);
+                  }
+               }
+            }
+
+            /*
+            if (RecordsOfCSVFile2.Count <= RecordsOfCSVFile1.Count) {
+               for (int i = 0; i <= RecordsOfCSVFile1.Count - 1; i++) {
+                  if (i < RecordsOfCSVFile2.Count) {
+                     var resultOfRecords = RecordsOfCSVFile2[i].Except(RecordsOfCSVFile1[i]).Union(RecordsOfCSVFile1[i].Except(RecordsOfCSVFile2[i])).ToList();
+                     if (resultOfRecords.Count > 0) {
+                        results.Add(RecordsOfCSVFile1[i]);
+                     }
+                  }
+                  else {
+                     results.Add(RecordsOfCSVFile1[i]);
+                  }
+               }
+
+               for (int i = 0; i <= RecordsOfCSVFile2.Count - 1; i++) {
+                  if (i < RecordsOfCSVFile1.Count) {
+                     var resultOfRecords = RecordsOfCSVFile1[i].Except(RecordsOfCSVFile2[i]).ToList();
+                     if (resultOfRecords.Count > 0) {
+                        results.Add(RecordsOfCSVFile2[i]);
+                     }
+                  }
+                  else {
+                     results.Add(RecordsOfCSVFile2[i]);
+                  }
+               }
+            }
+            else {
+               for (int i = 0; i <= RecordsOfCSVFile2.Count - 1; i++) {
+                  if (i < RecordsOfCSVFile1.Count) {
+                     var resultOfRecords = RecordsOfCSVFile2[i].Except(RecordsOfCSVFile1[i]).ToList();
+                     if (resultOfRecords.Count > 0) {
+                        results.Add(RecordsOfCSVFile1[i]);
+                     }
+                  }
+                  else {
+                     results.Add(RecordsOfCSVFile2[i]);
+
+                  }
+               }
+            }
+            */
+            if (results.Count == 0) {
+               MessageBox.Show("There is no difference between the two files being compared");
+               return;
+            }
+            DataTable dtDifferenceTable = ConvertListToDataTable(results);
+            dataGridView3.DataSource = dtDifferenceTable.DefaultView;
+            CsvWriter cw = new CsvWriter();
+
+            string fileName = CSVOutputFile;
+            //var t = new Thread((ThreadStart)(() =>
+            //{
+            //   SaveFileDialog saveFileDialogue = new SaveFileDialog();
+            //   saveFileDialogue.Filter = "CSV Files (*.csv)|*.csv|All files (*.*)|*.*;";// "CSV files (*.csv)";
+            //   if (saveFileDialogue.ShowDialog() == DialogResult.Cancel)
+            //      return;
+
+            //   fileName = saveFileDialogue.FileName;
+            //}));
+
+            //t.SetApartmentState(ApartmentState.STA);
+            //t.Start();
+            //t.Join();
+            if (!string.IsNullOrEmpty(fileName)) {
+               cw.WriteCsv(dtDifferenceTable, fileName, Encoding.Default);
+               MessageBox.Show("Data Exported");
+            }
+         }
+         catch (Exception) {
+            throw;
+         }
+      }
+
+      private CsvFile CreateCsvFile(List<string> headers, List<string> fields) {
+         CsvFile csvFile = new CsvFile();
+         headers.ForEach(header => csvFile.Headers.Add(header));
+         CsvRecord record = new CsvRecord();
+         fields.ForEach(field => record.Fields.Add(field));
+         csvFile.Records.Add(record);
+         return csvFile;
+      }
+
+      private DataTable ConvertListToDataTable(List<List<string>> list) {
+         // New table.
+         DataTable table = new DataTable();
+         table.TableName = "CSVTable";
+         // Get max columns.
+         int columns = 0;
+         columns = list[0].Count;
+
+         // Add columns.
+         for (int i = 0; i <= CSVFile1Headers.Count - 1; i++) {
+            table.Columns.Add(new DataColumn(CSVFile1Headers[i].ToString()));
+         }
+
+         // Add rows.
+         foreach (List<string> listString in list) {
+            object[] prams = new object[list[0].Count];
+            for (int i = 0; i <= CSVFile1Headers.Count - 1; i++) {
+               prams[i] = listString[i].ToString();
+            }
+            table.Rows.Add(prams);
+         }
+         return table;
+      }
+   }
+}
